1e1cacc77a1307890472eaab0fa2bc85873e0f7d
kateincoding <kateincoding@gmail.com>
2021-08-25 15:41:37 -0500

    [first sigaction, signal mod]

--- src/server.c
+++ src/server.c
@@ -6,7 +6,36 @@
 /*   By: kate <kate@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/25 07:34:17 by kate              #+#    #+#             */
-/*   Updated: 2021/08/25 07:34:18 by kate             ###   ########.fr       */
+/*   Updated: 2021/08/25 15:40:49 by kate             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
+void minitalk_handler(int sig)
+{
+    write(STDOUT_FILENO,"I will not die!\n", 16);
+}
+
+int main(void)
+{
+//    signal(SIGINT, &minitalk_handler);
+
+    struct sigaction    sa;
+    sigset_t            non_mask;
+
+    sa.sa_handler = 0;
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_mask = non_mask;
+	sa.sa_sigaction = minitalk_handler;
+    sigaction(SIGINT, &sa, NULL);
+    sigaction(SIGTERM, &sa, NULL);
+    while(1)
+    {
+        printf("hiiiii goku %d\n", getpid());
+        sleep(2);
+    }
+    return 0;
+}

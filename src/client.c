3f0b1a833b5502c9b748491383fd31a44e95cc32
kateincoding <kateincoding@gmail.com>
2021-08-26 02:36:26 -0500

    [signal connection]

--- src/client.c
+++ src/client.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kate <kate@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: ksoto <ksoto@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/25 07:34:09 by kate              #+#    #+#             */
-/*   Updated: 2021/08/25 23:55:05 by kate             ###   ########.fr       */
+/*   Created: 2021/08/25 07:34:09 by ksoto             #+#    #+#             */
+/*   Updated: 2021/08/26 02:33:31 by ksoto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,38 @@
 #include <unistd.h>
 #include <signal.h>
 
+int	ft_atoi(char *str)
+{
+	int		i;
+	int		sign;
+	long	number;
+
+	i = 0;
+	sign = 1;
+	number = 0;
+	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t'
+		|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		else
+			sign = 1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		number = (number * 10) + (str[i] - '0');
+		i++;
+	}
+	return (sign * number);
+}
+
+/*
+** handle_error - handle error when argc != 3
+*/
+
 void	handle_error(void)
 {
 	write(1, "usage: ./client [server-pid] [message]\n", 39);
@@ -22,13 +54,32 @@ void	handle_error(void)
 }
 
 /*
-** send_bits - function that send the message to the server bit per bit
+** send_bits - function that send the message to the server (ascii -> 8bits)
 ** @pid: id of process
 ** @msg: msg to send
 */
 
 int	send_bits(int pid, char *msg)
 {
+	int	bitmask;
+	int	i;
+
+	bitmask = -1;
+	i = 0;
+	while (msg[i])
+	{
+		while (++bitmask < 8)
+		{
+			if (msg[i] & 0x80 >> bitmask)
+				printf("1");
+			else
+				printf("0");
+			usleep(3);
+		}
+		i++;
+	}
+	printf("\n");
+	return (0);
 }
 
 /*
@@ -56,11 +107,14 @@ void	handle_client_signal(int sig)
 
 int	main(int argc, char **argv)
 {
+	int	pid;
+
 	if (argc != 3)
 		handle_error();
 	signal(SIGUSR1, handle_client_signal);
 	signal(SIGUSR2, handle_client_signal);
-	send_bits(ft_atoi(argv[1]), argv[2]);
+	pid = ft_atoi(argv[1]);
+	send_bits(pid, argv[2]);
 	while (1)
 		pause();
 }
--- src/server.c
+++ src/server.c
@@ -3,39 +3,72 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kate <kate@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: ksoto <ksoto@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/25 07:34:17 by kate              #+#    #+#             */
-/*   Updated: 2021/08/25 15:40:49 by kate             ###   ########.fr       */
+/*   Created: 2021/08/25 07:34:17 by ksoto             #+#    #+#             */
+/*   Updated: 2021/08/26 02:35:27 by ksoto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
 
-void minitalk_handler(int sig)
+/*
+** receive_signal - function that handle each case of signal
+** @sig: SIGUSR1 or SIGUSR2
+** @info: The siginfo_t structure is passed as the second parameter 
+** to a user signal handler function, if the SA_SIGINFO flag was 
+** specified when the handler was installed with sigaction().
+** @other: 
+*/
+
+void	receive_signal(int sig, siginfo_t *info, void *other)
 {
-    write(STDOUT_FILENO,"I will not die!\n", 16);
+	static char	c = 0xFF;
+	static int	bits = 0;
+
+	if (sig == SIGUSR1)
+	{
+		write (1, "0", 1);
+		c ^= 0x80 >> bits;
+	}
+	else if (sig == SIGUSR2)
+	{
+		write (1, "1", 1);
+		c |= 0x80 >> bits;
+	}
+	bits++;
+	if (bits == 8)
+	{
+		printf ("-> %c\n", c);
+		bits = 0;
+		c = 0xFF;
+	}
 }
 
-int main(void)
+/*
+** main - point of start the client program
+*/
+
+int	main(void)
 {
-//    signal(SIGINT, &minitalk_handler);
+	struct sigaction	sa;
+	sigset_t			non_mask;
+	pid_t				pid;
 
-    struct sigaction    sa;
-    sigset_t            non_mask;
-
-    sa.sa_handler = 0;
+	pid = getpid();
+	sa.sa_handler = 0;
 	sa.sa_flags = SA_SIGINFO;
 	sa.sa_mask = non_mask;
-	sa.sa_sigaction = minitalk_handler;
-    sigaction(SIGINT, &sa, NULL);
-    sigaction(SIGTERM, &sa, NULL);
-    while(1)
-    {
-        printf("hiiiii goku %d\n", getpid());
-        sleep(2);
-    }
-    return 0;
+	sa.sa_sigaction = receive_signal;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	// write(1, "PID: ", 5);
+	printf("PID: %d\n", pid);
+	// ft_putnbr_fd(getpid(), 1);
+	// write(1, "\n", 1);
+	while (1)
+		pause();
 }

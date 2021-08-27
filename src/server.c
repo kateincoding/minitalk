9fcaaca2372eff27e6d8b2581a48792407a6d855
kateincoding <kateincoding@gmail.com>
2021-08-26 19:48:57 -0500

    [signal->ascii->char]

--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: ksoto <ksoto@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/25 07:34:17 by ksoto             #+#    #+#             */
-/*   Updated: 2021/08/26 02:35:27 by ksoto            ###   ########.fr       */
+/*   Updated: 2021/08/26 19:48:17 by ksoto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,15 @@
 #include <unistd.h>
 #include <signal.h>
 
+void	error(int pid, char *str)
+{
+	if (str)
+		free(str);
+	write(STDERR_FILENO, "Error\n", 6);
+	//ft_putstr_fd("Unexpected error.\n", 2);
+	kill(pid, SIGUSR2);
+	exit(EXIT_FAILURE);
+}
 /*
 ** receive_signal - function that handle each case of signal
 ** @sig: SIGUSR1 or SIGUSR2
@@ -28,47 +37,49 @@ void	receive_signal(int sig, siginfo_t *info, void *other)
 {
 	static char	c = 0xFF;
 	static int	bits = 0;
+	static int	pid = 0;
+	static char *msg = 0;
 
+	(void)other;
 	if (sig == SIGUSR1)
-	{
-		write (1, "0", 1);
 		c ^= 0x80 >> bits;
-	}
 	else if (sig == SIGUSR2)
-	{
-		write (1, "1", 1);
 		c |= 0x80 >> bits;
-	}
-	bits++;
-	if (bits == 8)
+	if (++bits == 8)
 	{
 		printf ("-> %c\n", c);
 		bits = 0;
 		c = 0xFF;
 	}
+	if (kill(info->si_pid, SIGUSR1) == -1)
+		error(info->si_pid, msg);
 }
 
 /*
 ** main - point of start the client program
+** Functions election:
+** sigaction instead of signal to access to the pid of the sender: client 'info->si_pid'
+** infinite pause() loop waiting for signals from client
 */
 
 int	main(void)
 {
 	struct sigaction	sa;
 	sigset_t			non_mask;
-	pid_t				pid;
 
-	pid = getpid();
+	sigemptyset(&non_mask);
+	sigaddset(&non_mask, SIGINT);
+	sigaddset(&non_mask, SIGQUIT);
 	sa.sa_handler = 0;
 	sa.sa_flags = SA_SIGINFO;
 	sa.sa_mask = non_mask;
 	sa.sa_sigaction = receive_signal;
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
-	// write(1, "PID: ", 5);
-	printf("PID: %d\n", pid);
-	// ft_putnbr_fd(getpid(), 1);
-	// write(1, "\n", 1);
+	printf("PID: %d\n", getpid());
+	write(1, "PID: ", 5);
+//	ft_putnbr_fd(getpid(), 1);
+	write(1, "\n", 1);
 	while (1)
 		pause();
 }

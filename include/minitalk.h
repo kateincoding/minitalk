bb7e6e1d9db3c848fdb5a467f8fcfe874be23f80
kateincoding <kateincoding@gmail.com>
2021-08-27 22:35:29 -0500

    norminette

--- include/minitalk.h
+++ include/minitalk.h
@@ -6,17 +6,17 @@
 /*   By: ksoto <ksoto@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/25 07:12:48 by ksoto             #+#    #+#             */
-/*   Updated: 2021/08/27 21:53:38 by ksoto            ###   ########.fr       */
+/*   Updated: 2021/08/27 22:34:57 by ksoto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <signal.h>
-#include "libft.h"
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <signal.h>
+# include "libft.h"
 
 #endif
